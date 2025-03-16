a240014b569b31931b244cdc874f0752252914f5
i_ryandam <ryandam.9@gmail.com>
2025-03-16 19:47:29 +1100

    MCP Client to Docker server

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+```shell
+uv run "https://raw.githubusercontent.com/ryandam9/uv_tools/refs/heads/master/mcp_client_to_docker_servers.py" mcp/fetch-web
+```
+
--- mcp_client_to_docker_servers.py
+++ mcp_client_to_docker_servers.py
@@ -15,6 +15,12 @@ to work with MCP docker servers.
 To use this, you need to have the following installed:
     - Docker
     - uv
+
+Sample run
+==========
+uv run \
+    "https://raw.githubusercontent.com/ryandam9/uv_tools/refs/heads/master/mcp_client_to_docker_servers.py" \
+    mcp/time-zone-converter
 """
 
 import asyncio
